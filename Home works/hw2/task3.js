3d72d82968eac304c37de0ea8bfa5797a82a5451
mkose3 <104433296+mkose3@users.noreply.github.com>
2022-06-21 22:54:19 +0300

    solutıon for HW2

---
+++ Home works/hw2/task3.js
@@ -0,0 +1,8 @@
+// 3. Определить какое из трех, введенных пользователем, чисел максимальное и вывести его на экран.
+
+var a = Number(prompt("Введіть a:"));
+var b = Number(prompt("Введіть b:"));
+var c = Number(prompt("Введіть c:"));
+
+var result = Math.max(a ,b ,c );
+console.log(result);
